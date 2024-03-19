aae0196505c33c776cbb7dcdc1667e4c378ce618
Harsha <shharshacs@gmail.com>
2024-03-19 23:24:13 +0530

    first commit

---
+++ client/src/assets/index.js
@@ -0,0 +1,11 @@
+import download from './download.png';
+import logo from './logo.svg';
+import preview from './preview.png';
+import robot from './robot.jpg'
+
+export {
+  download,
+  logo,
+  preview,
+  robot
+};
